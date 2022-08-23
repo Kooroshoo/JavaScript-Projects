b6d2c9c79222ebb283b387b1314d872b51b1f114
Kourosh <kooroshoo@yahoo.com>
2022-08-23 05:47:21 +0200

    Update script.js

--- Dice Game/script.js
+++ Dice Game/script.js
@@ -1,21 +1,5 @@
 var randomNum = Math.floor( (Math.random() * 6) + 1 );
-var diceImage = document.querySelector(".img1");
+var randomDiceImage = "dice" + randomNum + ".png";
 
-if (randomNum === 1) {
-    diceImage.setAttribute("src", "images/dice1.png");
-} 
-else if (randomNum === 2) {
-    diceImage.setAttribute("src", "images/dice2.png");
-}
-else if (randomNum === 3) {
-    diceImage.setAttribute("src", "images/dice3.png");
-}
-else if (randomNum === 4) {
-    diceImage.setAttribute("src", "images/dice4.png");
-}
-else if (randomNum === 5) {
-    diceImage.setAttribute("src", "images/dice5.png");
-}
-else if (randomNum === 6) {
-    diceImage.setAttribute("src", "images/dice6.png");
-}
+var diceImage = document.querySelector(".img1");
+diceImage.setAttribute("src", "images/" + randomDiceImage);
