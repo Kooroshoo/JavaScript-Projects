df7cea4c883423aac99ee29e8726b2c7ef31660d
Kourosh <kooroshoo@yahoo.com>
2022-08-24 05:11:32 +0200

    Simon Game

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Small projects created with HTML, CSS, JavaScript & other frameworks. Click on t
 |---|---|
 |  [Dice Game](https://kooroshoo.github.io/JavaScript-Projects/Dice%20Game/index.html) | HTML, CSS, JavaScript |
 |  [Drum Kit](https://kooroshoo.github.io/JavaScript-Projects/Drum%20Kit/index.html) | HTML, CSS, JavaScript |
+|  [Simon Game](https://kooroshoo.github.io/JavaScript-Projects/Simon%20Game/index.html) | HTML, CSS, JavaScript, jQuery |
---
+++ Simon Game/script.js
@@ -0,0 +1,94 @@
+var gamePattern = [];
+var userClickedPattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gameHasStarted = false;
+var level = 0;
+
+
+$(document).keypress(function() {
+    if (gameHasStarted === false) {
+        gameHasStarted = true;
+        $("#level-title").text("Level " + level);
+        nextSequence();
+    }
+
+  });
+
+
+
+$( ".btn" ).click(function() {
+    
+    var userChosenColor = this.id;
+    var audio = new Audio("sounds/" + userChosenColor + ".mp3");
+    audio.play();
+    userClickedPattern.push(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+    
+
+});
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    playSound(randomChosenColor);
+    animatePress(randomChosenColor);
+
+    gamePattern.push(randomChosenColor);
+
+    var randomButton = $("#" + randomChosenColor);
+    randomButton.fadeOut(100).fadeIn(100);
+
+    level++;
+    $("#level-title").text("Level " + level);
+
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor) {
+    var selectedBtn = $("#" + currentColor);
+    selectedBtn.addClass("pressed");
+
+    setTimeout(function() {
+        selectedBtn.removeClass("pressed");
+      }, 100);
+
+}
+
+function checkAnswer(currentLevel) {
+
+    if (currentLevel === gamePattern.length -1) {
+        if (userClickedPattern[userClickedPattern.length-1] === gamePattern[gamePattern.length-1]) 
+        {
+            $("#level-title").text("Correct");
+            setTimeout(function() {
+                nextSequence();
+                userClickedPattern = [];
+              }, 1000);
+        } 
+        else 
+        {
+            var audio = new Audio("wrong.mp3");
+            audio.play();
+
+            $("body").addClass("game-over")
+            setTimeout(function() {
+                $("body").removeClass("game-over");
+            }, 200);
+
+            $("#level-title").text("Game Over, Press Any Key to Restart");
+            startOver();
+        }
+    }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    userClickedPattern = [];
+    gameHasStarted = false;
+}
